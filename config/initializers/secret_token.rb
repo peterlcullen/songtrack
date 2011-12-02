1092182c9eabd6c11c550a1bca259fa8fb9920c0
peterlcullen <peterlcullen@gmail.com>
2011-12-02 11:58:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songly2::Application.config.secret_token = 'd300a39c13905838f3b4f1d8ef4427a1442973c8b1517dcc8646a4fb5e95ed262e4149c04e1536b5405285048f9b13bdf4de1cef9e3c09ec95ac745def032254'
