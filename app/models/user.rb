8caba9f6ab1c1e0999bcb3ab000f70e2e6534474
peterlcullen <peterlcullen@gmail.com>
2011-12-05 00:48:25 -0600

    added stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
   attr_accessible :email, :username, :password, :password_confirmation
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates :email, :username, :uniqueness => true
   
   has_and_belongs_to_many :songs
 end
