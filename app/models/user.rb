90c6963d9e979fb0494efbee9b909a0d62550704
peterlcullen <peterlcullen@gmail.com>
2011-12-05 00:09:28 -0600

    added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :username, :password, :password_confirmation
   has_secure_password
   validates_presence_of :password, :on => :create
   
